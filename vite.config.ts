9e6369353f1afb7aea80598c16a69dcc57a52d08
lumay1 <lumayyingying@gmail.com>
2024-10-09 01:19:59 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/xiaoxiaole.new/',  // 添加这一行
+  base: '/xiaoxiaole.new/',
   plugins: [react()],
 })
