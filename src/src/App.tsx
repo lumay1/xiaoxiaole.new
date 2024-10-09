195f60c9ce5203ccd87e3ff14aa2abfe83752883
lumay1 <lumayyingying@gmail.com>
2024-10-09 20:15:57 +0800

    Create App.tsx

---
+++ src/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Game from './Game';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>消消乐游戏</h1>
+      <Game />
+    </div>
+  );
+};
+
+export default App;
