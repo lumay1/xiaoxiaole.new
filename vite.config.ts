b2d0c1a13725022b657083fb91eb5681d5d1bdd7
lumay1 <lumayyingying@gmail.com>
2024-10-09 19:34:29 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/xiaoxiaole.new/',
+  resolve: {
+    alias: {
+      '/src': '/src'
+    }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  }
 })
